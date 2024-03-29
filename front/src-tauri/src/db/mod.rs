d82c5a5099914095278b9d0595d895d2bb719ee2
NguyenTrongTin <ntrongtin11702@gmail.com>
2024-03-29 07:40:17 +0700

    feat: remove sled and bonsai db from frontend

--- front/src-tauri/src/db/mod.rs
+++ front/src-tauri/src/db/mod.rs
@@ -1,19 +1,16 @@
 use anyhow::{anyhow, Result};
-use sled::Db;
 use sqlx::{Executor, Row, SqlitePool};
 
-use crate::{cfg::DATABASE_PATH, model::RoomEntity};
+use crate::model::RoomEntity;
 
 mod sqlite;
 
 pub struct PoolWrapper {
-    // pub sled: Db,
-    pub sqlite: SqlitePool,
+    pool: SqlitePool,
 }
 
 impl PoolWrapper {
     pub async fn new() -> Self {
-        // let sled_pool = sled::open(DATABASE_PATH).unwrap();
         let sqlite_pool = sqlite::init_db()
             .await
             .expect("Failed to initialize SQLite database");
@@ -22,27 +19,24 @@ impl PoolWrapper {
             .await
             .expect("Failed to run initial SQL script");
 
-        PoolWrapper {
-            // sled: sled_pool,
-            sqlite: sqlite_pool,
-        }
+        PoolWrapper { pool: sqlite_pool }
     }
 
     pub async fn set_password(&self, password: &str) -> Result<()> {
         let _ = sqlx::query(
-            r#"INSERT INTO config (KEY, VALUE)
-            VALUES('pw', ?) ON CONFLICT (KEY)
-            DO UPDATE SET VALUE = excluded.VALUE;"#,
+            r#"insert into Config (key, value)
+            values('pw', ?) on conflict (key)
+            do update set value = excluded.value;"#,
         )
         .bind(password)
-        .execute(&self.sqlite)
+        .execute(&self.pool)
         .await?;
         Ok(())
     }
 
     pub async fn get_password(&self) -> Result<Option<String>> {
-        let row = sqlx::query(r#"select value from config where key = "pw";"#)
-            .fetch_optional(&self.sqlite)
+        let row = sqlx::query(r#"select value from Config where key = "pw";"#)
+            .fetch_optional(&self.pool)
             .await?;
         let val = match row {
             Some(r) => Some(r.try_get::<String, _>("value")?),
@@ -52,8 +46,8 @@ impl PoolWrapper {
     }
 
     pub async fn get_seed(&self) -> Result<Option<String>> {
-        let row = sqlx::query(r#"select value from config where key = "seed";"#)
-            .fetch_optional(&self.sqlite)
+        let row = sqlx::query(r#"select value from Config where key = "seed";"#)
+            .fetch_optional(&self.pool)
             .await?;
         let val = match row {
             Some(r) => Some(r.try_get::<String, _>("value")?),
@@ -62,7 +56,7 @@ impl PoolWrapper {
         Ok(val)
     }
 
-    pub fn add_or_update_room(&self, derivation_path: &str, room: &RoomEntity) -> Result<()> {
+    pub fn add_or_update_room(&self, deriv: &str, room: &RoomEntity) -> Result<()> {
         // let rooms_tree = self.sled.open_tree("rooms-".to_owned() + derivation_path)?;
         // let room_key = room.id.as_bytes();
         // let room_value = bincode::serialize(&room)?;
@@ -83,7 +77,7 @@ impl PoolWrapper {
         // Ok(rooms)
         Ok(vec![])
     }
-    pub fn get_room(&self, derivation_path: &str, room_id: &str) -> Result<RoomEntity> {
+    pub fn get_room(&self, deriv: &str, room_id: &str) -> Result<RoomEntity> {
         // let rooms_tree = self.sled.open_tree("rooms-".to_owned() + derivation_path)?;
         // if let Ok(Some(room)) = rooms_tree.get(room_id) {
         //     let room: RoomEntity = bincode::deserialize(&room)?;
